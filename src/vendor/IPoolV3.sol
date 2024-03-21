a525bcdac78f8714d9b81c57c3cee30b001fe0d9
amarcu <alx.marcu@gmail.com>
2024-03-21 09:28:49 +0200

    CDPCreditVault

---
+++ src/vendor/IPoolV3.sol
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: MIT
+// Gearbox Protocol. Generalized leverage for DeFi protocols
+// (c) Gearbox Foundation, 2023.
+pragma solidity ^0.8.17;
+pragma abicoder v1;
+
+import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+import {IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
+import {IVersion} from "@gearbox-protocol/core-v2/contracts/interfaces/IVersion.sol";
+
+interface IPoolV3Events {
+    /// @notice Emitted when depositing liquidity with referral code
+    event Refer(address indexed onBehalfOf, uint256 indexed referralCode, uint256 amount);
+
+    /// @notice Emitted when credit account borrows funds from the pool
+    event Borrow(address indexed creditManager, address indexed creditAccount, uint256 amount);
+
+    /// @notice Emitted when credit account's debt is repaid to the pool
+    event Repay(address indexed creditManager, uint256 borrowedAmount, uint256 profit, uint256 loss);
+
+    /// @notice Emitted when incurred loss can't be fully covered by burning treasury's shares
+    event IncurUncoveredLoss(address indexed creditManager, uint256 loss);
+
+    /// @notice Emitted when new interest rate model contract is set
+    event SetInterestRateModel(address indexed newInterestRateModel);
+
+    /// @notice Emitted when new pool quota keeper contract is set
+    event SetPoolQuotaKeeper(address indexed newPoolQuotaKeeper);
+
+    /// @notice Emitted when new total debt limit is set
+    event SetTotalDebtLimit(uint256 limit);
+
+    /// @notice Emitted when new credit manager is connected to the pool
+    event AddCreditManager(address indexed creditManager);
+
+    /// @notice Emitted when new debt limit is set for a credit manager
+    event SetCreditManagerDebtLimit(address indexed creditManager, uint256 newLimit);
+
+    /// @notice Emitted when new withdrawal fee is set
+    event SetWithdrawFee(uint256 fee);
+
+	/// @notice Emitter when redeeming Underlying tokens for internal credit
+	event Enter(address indexed user, uint256 amount);
+
+	/// @notice Emitter when redeeming internal credit for Underlying tokens
+    event Exit(address indexed user, uint256 amount);
+}
+
+/// @title Pool V3 interface
+interface IPoolV3 is IVersion, IPoolV3Events, IERC4626, IERC20Permit {
+    function addressProvider() external view returns (address);
+
+    function underlyingToken() external view returns (address);
+
+    function treasury() external view returns (address);
+
+	function cdm() external view returns (address);
+
+    function withdrawFee() external view returns (uint16);
+
+    function availableLiquidity() external view returns (uint256);
+
+    function expectedLiquidity() external view returns (uint256);
+
+    function expectedLiquidityLU() external view returns (uint256);
+
+    // ---------------- //
+    // ERC-4626 LENDING //
+    // ---------------- //
+
+    function depositWithReferral(uint256 assets, address receiver, uint256 referralCode)
+        external
+        returns (uint256 shares);
+
+    function mintWithReferral(uint256 shares, address receiver, uint256 referralCode)
+        external
+        returns (uint256 assets);
+
+
+	function enter(address user, uint256 amount) external;
+	function exit(address user, uint256 amount) external;
+
+    // ------------- //
+    // INTEREST RATE //
+    // ------------- //
+
+    function interestRateModel() external view returns (address);
+
+    function baseInterestRate() external view returns (uint256);
+
+    function supplyRate() external view returns (uint256);
+
+    function baseInterestIndex() external view returns (uint256);
+
+    function baseInterestIndexLU() external view returns (uint256);
+
+    function lastBaseInterestUpdate() external view returns (uint40);
+
+    // ------ //
+    // QUOTAS //
+    // ------ //
+
+    function poolQuotaKeeper() external view returns (address);
+
+    function quotaRevenue() external view returns (uint256);
+
+    function lastQuotaRevenueUpdate() external view returns (uint40);
+
+    function updateQuotaRevenue(int256 quotaRevenueDelta) external;
+
+    function setQuotaRevenue(uint256 newQuotaRevenue) external;
+
+    // ------------- //
+    // CONFIGURATION //
+    // ------------- //
+
+    function setInterestRateModel(address newInterestRateModel) external;
+
+    function setPoolQuotaKeeper(address newPoolQuotaKeeper) external;
+
+    function setTotalDebtLimit(uint256 newLimit) external;
+
+    function setWithdrawFee(uint256 newWithdrawFee) external;
+}

dade823b77eb60106638c574066880c0141f8239
amarcu <alx.marcu@gmail.com>
2024-09-23 17:06:24 +0300

    Typo fix

--- src/CDPVault.sol
+++ src/CDPVault.sol
@@ -522,7 +522,8 @@ contract CDPVault is AccessControl, Pause, Permission, ICDPVaultBase {
         uint256 spotPrice_ = spotPrice();
         uint256 discountedPrice = wmul(spotPrice_, liqConfig_.liquidationDiscount);
         if (spotPrice_ == 0) revert CDPVault__liquidatePosition_invalidSpotPrice();
-        // Enusure that there's no bad debt
+        
+        // Ensure that there's no bad debt
         if (calcTotalDebt(debtData) > wmul(position.collateral, spotPrice_)) revert CDPVault__BadDebt();
 
         // compute collateral to take, debt to repay and penalty to pay

190027b6ecaa60f421bfb2f9c934e3e00fc40df0
0xtj24 <tabboz23@proton.me>
2024-09-24 19:27:41 +0200

    add Locking contract and test

---
+++ src/Locking.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract Locking {
+    using SafeERC20 for IERC20;
+
+    address public owner;
+    uint256 public cooldownPeriod;
+    IERC20 public token;
+
+    struct Deposit {
+        uint256 amount;
+        uint256 cooldownStart;
+        uint256 cooldownAmount;
+    }
+
+    mapping(address => Deposit) public deposits;
+
+    event Deposited(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+    event CooldownPeriodSet(uint256 cooldownPeriod);
+    event CooldownInitiated(address indexed user, uint256 timestamp, uint256 amount);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can perform this action");
+        _;
+    }
+
+    constructor(address _token) {
+        owner = msg.sender;
+        token = IERC20(_token);
+    }
+
+    function setCooldownPeriod(uint256 _cooldownPeriod) external onlyOwner {
+        cooldownPeriod = _cooldownPeriod;
+        emit CooldownPeriodSet(_cooldownPeriod);
+    }
+
+    function deposit(uint256 _amount) external {
+        require(_amount > 0, "Must send some tokens to deposit");
+
+        deposits[msg.sender].amount += _amount;
+        token.safeTransferFrom(msg.sender, address(this), _amount);
+
+        emit Deposited(msg.sender, _amount);
+    }
+
+    function initiateCooldown(uint256 _amount) external {
+        require(deposits[msg.sender].amount >= _amount, "Insufficient balance to set cooldown amount");
+        deposits[msg.sender].cooldownStart = block.timestamp;
+        deposits[msg.sender].cooldownAmount += _amount;
+
+        emit CooldownInitiated(msg.sender, block.timestamp, _amount);
+    }
+
+    function withdraw() external {
+        if (cooldownPeriod > 0) {
+            require(
+                block.timestamp >= deposits[msg.sender].cooldownStart + cooldownPeriod,
+                "Cooldown period has not passed"
+            );
+        }
+
+        uint256 _amount = deposits[msg.sender].cooldownAmount;
+        deposits[msg.sender].amount -= _amount;
+        deposits[msg.sender].cooldownAmount = 0;
+        deposits[msg.sender].cooldownStart = 0;
+        token.safeTransfer(msg.sender, _amount);
+
+        emit Withdrawn(msg.sender, _amount);
+    }
+
+    function getBalance(address _user) external view returns (uint256) {
+        return deposits[_user].amount;
+    }
+
+    function getCoolDownInfo(address _user) external view returns (uint256, uint256, uint256) {
+        return (deposits[_user].amount, deposits[_user].cooldownStart, deposits[_user].cooldownAmount);
+    }
+}

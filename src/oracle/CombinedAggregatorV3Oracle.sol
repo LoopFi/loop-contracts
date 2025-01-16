4fe502d602c0d5a21aa4f3c9011cab1c7e88e764
0xtj24 <tabboz23@proton.me>
2025-01-16 15:58:56 -0500

    simplify CombinedAggregatorV3Oracle

--- src/oracle/CombinedAggregatorV3Oracle.sol
+++ src/oracle/CombinedAggregatorV3Oracle.sol
@@ -12,9 +12,10 @@ contract CombinedAggregatorV3Oracle {
 
     AggregatorV3Interface public immutable aggregator;
     uint256 public immutable aggregatorHeartbeat;
+    uint256 public immutable aggregatorScale;
+
     AggregatorV3Interface public immutable aggregator2;
     uint256 public immutable aggregator2Heartbeat;
-    uint256 public immutable aggregatorScale;
     uint256 public immutable aggregator2Scale;
 
     constructor(address _aggregator, uint256 _aggregatorHeartbeat, address _aggregator2, uint256 _aggregator2Heartbeat) {        
@@ -27,11 +28,11 @@ contract CombinedAggregatorV3Oracle {
         aggregator2Heartbeat = _aggregator2Heartbeat;
     }
 
-    function getAggregatorData() public view returns (uint256, uint256) {
-        (, int256 answer, , uint256 updatedAt,) = aggregator.latestRoundData();
-        bool isValid = (answer > 0 && block.timestamp - updatedAt <= aggregatorHeartbeat);
-        if (!isValid) revert CombinedAggregatorV3Oracle__invalidAggregatorValue(address(aggregator));
-        return (wdiv(uint256(answer), aggregatorScale), uint256(updatedAt));
+    function getAggregatorData(AggregatorV3Interface _aggregator, uint256 _aggregatorScale, uint256 _aggregatorHeartbeat) public view returns (uint256, uint256) {
+        (, int256 answer, , uint256 updatedAt,) = _aggregator.latestRoundData();
+        bool isValid = (answer > 0 && block.timestamp - updatedAt <= _aggregatorHeartbeat);
+        if (!isValid) revert CombinedAggregatorV3Oracle__invalidAggregatorValue(address(_aggregator));
+        return (wdiv(uint256(answer), _aggregatorScale), updatedAt);
     }
 
     /// @notice Return the latest price combined from two Chainlink like oracle and the timestamp from the first aggregator
@@ -40,27 +41,12 @@ contract CombinedAggregatorV3Oracle {
         view
         returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
     {
-        (uint256 value, uint256 timestamp) = getAggregatorData();
-        (bool isValid, uint256 price) = _fetchAndValidate();
-        if (!isValid) revert CombinedAggregatorV3Oracle__invalidAggregatorValue(address(aggregator2));
-        price = wmul(uint256(value), price);
+        (uint256 value, uint256 timestamp) = getAggregatorData(aggregator, aggregatorScale, aggregatorHeartbeat);
+        (uint256 value2, ) = getAggregatorData(aggregator2, aggregator2Scale, aggregator2Heartbeat);
+        uint256 price = wmul(value, value2);
         return (0, int256(price), 0, timestamp, 0);
     }
 
-    function _fetchAndValidate() internal view returns (bool isValid, uint256 price) {
-        try aggregator2.latestRoundData() returns (
-            uint80,
-            int256 answer,
-            uint256 /*startedAt*/,
-            uint256 updatedAt,
-            uint80
-        ) {
-            isValid = (answer > 0 && block.timestamp - updatedAt <= aggregator2Heartbeat);
-            return (isValid, wdiv(uint256(answer), aggregator2Scale));
-        } catch {
-            // return the default values (false, 0) on failure
-        }
-    }
 
     function decimals() public pure returns (uint256) {
         return 18;
